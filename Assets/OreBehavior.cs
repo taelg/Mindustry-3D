6ea8092faf97d71f411efff66941065db6356689
taelgalli <taelgalli@gmail.com>
2024-07-20 15:10:10 -0300

    Resolve Conflits in OreBehavior

--- Assets/OreBehavior.cs
+++ Assets/OreBehavior.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using UnityEngine;
 
 public class OreBehavior : MonoBehaviour {
@@ -18,24 +17,6 @@ public class OreBehavior : MonoBehaviour {
 
     public OreType GetOreType() {
         return oreType;
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class OreBehavior : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        RandomizeRotation();
-
-    }
-
-    private void RandomizeRotation() {
-        float rotationX = RandomUtils.RandomRotation();
-        float rotationY = RandomUtils.RandomRotation();
-        float rotationZ = RandomUtils.RandomRotation();
-        this.transform.rotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
->>>>>>> b97cff57c6282d54b53a08d536f381b8798264a1
     }
 
 
