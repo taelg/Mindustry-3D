86fd0b42a877548bf092b45c64539e0e88c0d08f
taelgalli <taelgalli@gmail.com>
2024-07-27 18:07:37 -0300

    Add feature do drag and drop during place mode

--- Assets/Placeables/Scripts/PlaceModeManager.cs
+++ Assets/Placeables/Scripts/PlaceModeManager.cs
@@ -8,19 +8,18 @@ public class PlaceModeManager : SingletonBehavior<PlaceModeManager> {
 
     private bool isActive = false;
     private GameObject placeableSelected;
+    private BoxCollider placeableBoxCollider;
+    private Vector3 dragInitialPos;
+    private Vector3 dragFinalPos;
+    private bool isDragging = false;
 
     private new void Awake() {
         base.Awake();
     }
 
-    private void Update() {
-        UpdateItemPosition();
-        OnScrollRotateItem();
-        OnClickAddItem();
-    }
-
     public void StartMode(GameObject placeableSelected) {
         this.placeableSelected = placeableSelected;
+        this.placeableBoxCollider = placeableSelected.GetComponent<BoxCollider>();
         isActive = true;
         placeableSelected.SetActive(true);
         activeCamera.GetComponent<CameraZoomBehavior>().enabled = false;
@@ -35,35 +34,90 @@ public class PlaceModeManager : SingletonBehavior<PlaceModeManager> {
         }
     }
 
-    private void UpdateItemPosition() {
+    private void Update() {
+        HandleInputs();
+        MovePlaceableToMouseWorldPosition();
+    }
+
+    private void MovePlaceableToMouseWorldPosition() {
         if (isActive) {
             Vector3 position = RaycastUtils.GetMouseWorldPosition(activeCamera);
-            BoxCollider boxCollider = placeableSelected.GetComponent<BoxCollider>();
-            position = GetPositionSnappedToGrid(position, boxCollider.size);
-            position = GetFixedHeight(position, 0);
+            position = GetPositionSnappedToGrid(position);
             placeableSelected.transform.position = position;
         }
     }
 
-    private Vector3 GetFixedHeight(Vector3 pos, int height) {
-        return new Vector3(pos.x, height, pos.z);
+    private void HandleInputs() {
+        if (isActive) {
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            if (scroll != 0)
+                OnScrollRotatePlaceable(scroll);
+
+            if (Input.GetMouseButtonDown(0) && !RaycastUtils.IsMouseOverUI())
+                OnStartDrag();
+
+            if (Input.GetMouseButtonUp(0))
+                OnEndDrag();
+        }
     }
 
-    private Vector3 GetPositionSnappedToGrid(Vector3 position, Vector3 size) {
+    private void OnScrollRotatePlaceable(float scroll) {
+        float scrollDir = scroll > 0 ? -1 : 1;
+        placeableSelected.transform.Rotate(Vector3.up, scrollDir * 90);
+    }
+    private void OnStartDrag() {
+        isDragging = true;
+        dragInitialPos = placeableSelected.transform.position;
+    }
+
+    private void OnEndDrag() {
+        if (isDragging) {
+            isDragging = false;
+            dragFinalPos = placeableSelected.transform.position;
+            OnEndDragAddPlaceableToWorld();
+            Debug.DrawLine(dragInitialPos, dragFinalPos, Color.red, 5);
+        }
+    }
+
+    private void OnEndDragAddPlaceableToWorld() {
+        Vector3 drag = GetDrag();
+        Vector3 placeableSize = placeableBoxCollider.size;
+        Vector3 currentPos = dragInitialPos;
+        int totalPlaceables = drag.x != 0
+            ? Mathf.CeilToInt(Mathf.Abs(dragFinalPos.x - dragInitialPos.x) / placeableSize.x)
+            : Mathf.CeilToInt(Mathf.Abs(dragFinalPos.z - dragInitialPos.z) / placeableSize.z);
+
+        for (int i = 0; i <= totalPlaceables; i++) {
+            if (IsGridEmptyAtCurrentPos(currentPos, placeableSize)) {
+                GameObject gameObject = Instantiate(placeableSelected, currentPos, placeableSelected.transform.rotation, placeableParent);
+                TakeSpace(gameObject.transform, placeableBoxCollider);
+            }
+            currentPos += drag * placeableSize.x;
+        }
+
+    }
+
+    private Vector3 GetDrag() {
+        Vector3 direction = dragFinalPos - dragInitialPos;
+        float distanceX = Mathf.Abs(direction.x);
+        float distanceZ = Mathf.Abs(direction.z);
+
+        if (distanceX > distanceZ) {
+            direction = new Vector3(Mathf.Sign(direction.x), 0, 0);
+        } else {
+            direction = new Vector3(0, 0, Mathf.Sign(direction.z));
+        }
+        return direction;
+    }
+
+    private Vector3 GetPositionSnappedToGrid(Vector3 position) {
+        Vector3 size = placeableBoxCollider.size;
         float gridCellSize = 1;
         Vector3 halfSize = size * 0.5f;
         float x = Mathf.Round((position.x - halfSize.x) / gridCellSize) * gridCellSize + halfSize.x;
         float z = Mathf.Round((position.z - halfSize.z) / gridCellSize) * gridCellSize + halfSize.z;
-        Vector3 snappedPosition = new Vector3(x, position.y, z);
+        Vector3 snappedPosition = new Vector3(x, 0, z);
         return snappedPosition;
-    }
-
-    private void OnScrollRotateItem() {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (isActive && scroll != 0) {
-            scroll = scroll > 0 ? 1 : -1;
-            placeableSelected.transform.Rotate(Vector3.up, scroll * 90);
-        }
     }
 
     public void TakeSpace(Transform transform, BoxCollider boxCollider) {
@@ -77,10 +131,7 @@ public class PlaceModeManager : SingletonBehavior<PlaceModeManager> {
         }
     }
 
-    private bool IsGridEmptyAtCurrentPos() {
-        Vector3 position = placeableSelected.transform.position;
-        BoxCollider boxCollider = placeableSelected.GetComponent<BoxCollider>();
-        Vector3 size = boxCollider.size;
+    private bool IsGridEmptyAtCurrentPos(Vector3 position, Vector3 size) {
         Vector3 bottomLeft = position - size * 0.5f;
 
         for (int x = Mathf.FloorToInt(bottomLeft.x); x < Mathf.CeilToInt(bottomLeft.x + size.x); x++) {
@@ -94,22 +145,5 @@ public class PlaceModeManager : SingletonBehavior<PlaceModeManager> {
         }
         return true;
     }
-
-    private void OnClickAddItem() {
-        if (isActive) {
-            bool clickToAdd = Input.GetMouseButtonDown(0);
-            bool notInMenu = !RaycastUtils.IsMouseOverUI();
-            bool isFreeSpace = IsGridEmptyAtCurrentPos();
-
-            if (clickToAdd && notInMenu && isFreeSpace) {
-                GameObject gameObject = Instantiate(placeableSelected, placeableSelected.transform.position, placeableSelected.transform.rotation, placeableParent);
-                BoxCollider boxCollider = gameObject.transform.GetComponent<BoxCollider>();
-                TakeSpace(gameObject.transform, boxCollider);
-            }
-        }
-    }
-
-
-
 
 }
